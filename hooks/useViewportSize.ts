7e0bf73d551f4827118acc76a21c8fc278967b2b
Brandon Smith <mail@brandons.me>
2023-01-19 18:54:05 -0600

    Fix hydration bug

--- hooks/useViewportSize.ts
+++ hooks/useViewportSize.ts
@@ -1,21 +1,18 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { isClientSide } from "../utils/misc";
 
 export default function useViewportSize() {
-    const [viewportSize, setViewportSize] = useState(
-        isClientSide()
-            ? { width: window.innerWidth, height: window.innerHeight }
-            : { width: 0, height: 0 }
-    )
+    const [viewportSize, setViewportSize] = useState({ width: 0, height: 0 })
 
     const updateViewportSize = useCallback(() => {
         setViewportSize({ width: window.innerWidth, height: window.innerHeight })
     }, [])
 
     useEffect(() => {
+        updateViewportSize()
         window.addEventListener('resize', updateViewportSize)
         return () => window.removeEventListener('resize', updateViewportSize)
-    })
+    }, [])
 
     return viewportSize
 }
