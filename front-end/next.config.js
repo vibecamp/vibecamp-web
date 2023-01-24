57db02de90d0f98585aec6b1a0dec84c8c84361c
Brandon Smith <mail@brandons.me>
2023-01-24 00:04:34 -0600

    Fetch page data from back-end

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -1,11 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     externalDir: true,
   },
   publicRuntimeConfig: {
-    BACK_END_ORIGIN: process.env.BACK_END_ORIGIN ?? 'http://localhost:10000'
+    BACK_END_ORIGIN: process.env.BACK_END_ORIGIN ?? 'http://127.0.0.1:10000'
   },
 }
 
