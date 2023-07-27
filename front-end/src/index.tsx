48c6ef0b662f6c42ad7e6ec173b5c0c1f383174a
Brandon Smith <mail@brandons.me>
2023-07-27 17:50:37 -0500

    Register the service worker

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './components/App'
 
 window.addEventListener('load', () => {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js')
+    }
+
     const root = createRoot(document.getElementById('root') as HTMLElement)
     root.render(<App />)
 })
