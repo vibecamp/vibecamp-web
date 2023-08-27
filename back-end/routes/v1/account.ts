85b33da44311e4bb919c51977d4d3da1f6a7464d
Brandon Smith <mail@brandons.me>
2023-08-27 17:18:07 -0500

    Load all account info from /account endpoint

--- back-end/routes/v1/account.ts
+++ back-end/routes/v1/account.ts
@@ -1,23 +1,72 @@
 import { Router, Status } from "oak";
 import { defineRoute } from "./_common.ts";
 import { db } from "../../db.ts";
-import { Account } from "../../db.d.ts";
+import { FullAccountInfo } from "https://raw.githubusercontent.com/vibecamp/vibecamp-web/main/common/data.ts";
 
 export default function register(router: Router) {
     const baseRoute = '/account'
 
     // purchase one or multiple tickets, fill out baseline required attendee info
-    defineRoute<Account | null>(router, {
+    defineRoute<FullAccountInfo | null>(router, {
         endpoint: baseRoute,
         method: 'get',
         requireAuth: true,
         handler: async (_ctx, jwt) => {
             const { account_id } = jwt
 
-            const account = await db.selectFrom('account').where('account_id', '=', account_id).selectAll().executeTakeFirst()
+            const rows = await db
+                .selectFrom('account')
+                .leftJoin('account_attendee', 'account.account_id', 'account_attendee.account_id')
+                .leftJoin('attendee', 'account_attendee.attendee_id', 'attendee.attendee_id')
+                .leftJoin('ticket', 'attendee.ticket_id', 'ticket.ticket_id')
+                .where('account.account_id', '=', account_id)
+                .selectAll()
+                .execute()
 
-            return [account ?? null, Status.OK]
+            const firstRow = rows[0]
+            if (firstRow != null) {
+                const { account_id, email_address } = firstRow
+                const attendees = new Map<number, FullAccountInfo['attendees'][number]>()
+
+                for (const row of rows) {
+                    const attendee_id = row.attendee_id as number
+
+                    if (!attendees.has(attendee_id)) {
+                        const {
+                            name,
+                            is_child,
+                            dietary_restrictions,
+                            has_purchased_bedding,
+                            has_purchased_bus_ticket,
+                            is_default_for_account,
+                            ticket_id,
+                            event_id
+                        } = row
+
+                        attendees.set(attendee_id, {
+                            attendee_id,
+                            name,
+                            is_child,
+                            dietary_restrictions,
+                            has_purchased_bedding,
+                            has_purchased_bus_ticket,
+                            is_default_for_account,
+                            ticket: {
+                                ticket_id,
+                                event_id
+                            }
+                        })
+                    }
+                }
+
+                return [{
+                    account_id,
+                    email_address,
+                    attendees: Array.from(attendees.values())
+                }, Status.OK]
+            }
+
+            return [null, Status.NotFound]
         }
     })
-
 }
--- common/data.ts
+++ common/data.ts
@@ -1,12 +1,30 @@
 
 export type VibeJWTPayload = {
-    iss?: string;
-    sub?: string;
-    aud?: string[] | string;
-    exp?: number;
-    nbf?: number;
-    iat?: number;
-    jti?: string;
-    [key: string]: unknown;
+    iss?: string,
+    sub?: string,
+    aud?: string[] | string,
+    exp?: number,
+    nbf?: number,
+    iat?: number,
+    jti?: string,
+    [key: string]: unknown,
     account_id: number
+}
+
+export type FullAccountInfo = {
+    account_id: number,
+    email_address: string,
+    attendees: Array<{
+        attendee_id: number,
+        name: string | null,
+        is_child: boolean | null,
+        dietary_restrictions: string | null,
+        has_purchased_bedding: boolean | null,
+        has_purchased_bus_ticket: string | null,
+        is_default_for_account: boolean | null,
+        ticket: {
+            ticket_id: number | null,
+            event_id: number | null,
+        }
+    }>
 }
