d7a68a2e9480aed92ec040835f33044130ee996c
Brandon Smith <mail@brandons.me>
2023-10-25 21:46:18 -0500

    Update DB types

--- back-end/db-generate-types.ts
+++ back-end/db-generate-types.ts
@@ -10,72 +10,80 @@ type DataType =
     | 'point'
     | 'uuid'
 
-const columns = await withDBConnection(async db => (await db.queryObject<{
-    table_name: string,
-    column_name: string,
-    data_type: DataType,
-    is_nullable: 'YES' | 'NO'
-}>`
+await withDBConnection(async db => {
+
+    const columns = (await db.queryObject<{
+        table_name: string,
+        column_name: string,
+        data_type: DataType,
+        is_nullable: 'YES' | 'NO'
+    }>`
     SELECT information_schema.tables.table_name, column_name, data_type, is_nullable
     FROM information_schema.tables, information_schema.columns
     WHERE information_schema.tables.table_schema = 'public' AND information_schema.tables.table_name = information_schema.columns.table_name
     ORDER BY information_schema.tables.table_name, information_schema.columns.column_name ASC
-`).rows)
+`).rows
 
-/**
- * Postgres column type -> TypeScript type
- */
-const TYPE_MAP: Record<DataType, string> = {
-    'boolean': 'boolean',
-    'text': 'string',
-    'uuid': 'string',
-    'integer': 'number',
-    'point': 'unknown',
-    'date': 'Date',
-    'timestamp with time zone': 'unknown'
-}
+    // dumped table content
+    const tableRowsToDump: readonly string[] = [
+        'purchase_type',
+        'volunteer_type',
+        'age_group',
+        'diet'
+    ]
 
-const tables: Record<string, Array<{ column_name: string, type: string }>> = {}
-for (const { table_name, column_name, data_type, is_nullable } of columns) {
-    if (tables[table_name] == null) {
-        tables[table_name] = []
+    const tableRows: Record<string, Array<unknown>> = Object.fromEntries(await Promise.all(
+        tableRowsToDump
+            .map(async table_name =>
+                [table_name, await db.queryTable(table_name as any)] as const)
+    ))
+
+    const dbRowsStr =
+        `export const TABLE_ROWS = {
+${Object.entries(tableRows).map(([tableName, rows]) =>
+            `  ${tableName}: [
+${rows.map(row => `    ${JSON.stringify(row)},`).join('\n')}
+  ],`).join('\n')}
+} as const`
+
+    // table schemas
+    const tables: Record<string, Array<{ column_name: string, type: string }>> = {}
+
+    /**
+     * Postgres column type -> TypeScript type
+     */
+    const TYPE_MAP: Record<DataType, string> = {
+        'boolean': 'boolean',
+        'text': 'string',
+        'uuid': 'string',
+        'integer': 'number',
+        'point': 'unknown',
+        'date': 'Date',
+        'timestamp with time zone': 'unknown'
     }
 
-    tables[table_name]?.push({
-        column_name,
-        type: TYPE_MAP[data_type] + (is_nullable === 'YES' ? ' | null' : '')
-    })
-}
+    for (const { table_name, column_name, data_type, is_nullable } of columns) {
+        if (tables[table_name] == null) {
+            tables[table_name] = []
+        }
 
-const dbTypesStr =
-    `export type Tables = {
+        tables[table_name]?.push({
+            column_name,
+            type: TYPE_MAP[data_type] + (is_nullable === 'YES' ? ' | null' : '')
+        })
+    }
+
+    const dbTypesStr =
+        `export type Tables = {
 ${Object.entries(tables).map(([tableName, columns]) =>
-        `  ${tableName}: {
+            tableRowsToDump.includes(tableName)
+                ? `  ${tableName}: (typeof TABLE_ROWS)['${tableName}'][number]`
+                : `  ${tableName}: {
 ${columns.map(({ column_name, type }) => `    ${column_name}: ${type},`).join('\n')}
   },`).join('\n')}
 }
 
 export type TableName = keyof Tables`
 
-const tableRowsToDump = [
-    'purchase_type',
-    'volunteer_type',
-    'age_group'
-] as const
-
-const tableRows: Record<string, Array<unknown>> = Object.fromEntries(await Promise.all(
-    tableRowsToDump
-        .map(table_name =>
-            withDBConnection(async db =>
-                [table_name, await db.queryTable(table_name)] as const))
-))
-
-const dbRowsStr =
-    `export const TABLE_ROWS = {
-${Object.entries(tableRows).map(([tableName, rows]) =>
-        `  ${tableName}: [
-${rows.map(row => `    ${JSON.stringify(row)},`).join('\n')}
-  ],`).join('\n')}
-} as const`
-
-await Deno.writeTextFile('./db-types.ts', dbTypesStr + '\n\n' + dbRowsStr)
+    await Deno.writeTextFile('./db-types.ts', dbTypesStr + '\n\n' + dbRowsStr)
+})
--- back-end/db-types.ts
+++ back-end/db-types.ts
@@ -8,25 +8,30 @@ export type Tables = {
     password_hash: string,
     password_salt: string,
   },
-  age_group: {
-    age_group: string,
-    description: string,
-    is_child: boolean,
-  },
+  age_group: (typeof TABLE_ROWS)['age_group'][number]
   attendee: {
     age_group: string | null,
     associated_account_id: number,
     attendee_id: number,
-    dietary_restrictions: string,
     discord_handle: string | null,
+    has_allergy_eggs: boolean,
+    has_allergy_fish: boolean,
+    has_allergy_milk: boolean,
+    has_allergy_peanuts: boolean,
+    has_allergy_shellfish: boolean,
+    has_allergy_soy: boolean,
+    has_allergy_tree_nuts: boolean,
+    has_allergy_wheat: boolean,
     interested_in_pre_call: boolean,
     interested_in_volunteering_as: string | null,
     medical_training: string | null,
     name: string | null,
     notes: string,
     planning_to_camp: boolean,
+    special_diet: string | null,
     twitter_handle: string | null,
   },
+  diet: (typeof TABLE_ROWS)['diet'][number]
   event: {
     created_by_account_id: number,
     description: string,
@@ -69,37 +74,31 @@ export type Tables = {
     purchase_type_id: string,
     purchased_on: unknown,
   },
-  purchase_type: {
-    description: string,
-    festival_id: number,
-    max_available: number | null,
-    max_per_account: number | null,
-    price_in_cents: number,
-    purchase_type_id: string,
-  },
-  volunteer_type: {
-    description: string,
-    volunteer_type_id: string,
-  },
+  purchase_type: (typeof TABLE_ROWS)['purchase_type'][number]
+  volunteer_type: (typeof TABLE_ROWS)['volunteer_type'][number]
 }
 
 export type TableName = keyof Tables
 
 export const TABLE_ROWS = {
   purchase_type: [
-    {"purchase_type_id":"BUS_VIBECLIPSE_2024","price_in_cents":1200,"festival_id":1,"max_available":null,"description":"Bus ticket (two-way)","max_per_account":null},
-    {"purchase_type_id":"SLEEPING_BAG_VIBECLIPSE_2024","price_in_cents":1200,"festival_id":1,"max_available":null,"description":"Sleeping bag","max_per_account":null},
-    {"purchase_type_id":"ATTENDANCE_VIBECLIPSE_2024","price_in_cents":12300,"festival_id":1,"max_available":600,"description":"Adult Ticket","max_per_account":2},
-    {"purchase_type_id":"ATTENDANCE_CHILD_VIBECLIPSE_2024","price_in_cents":12300,"festival_id":1,"max_available":null,"description":"Child Ticket","max_per_account":4},
+    { "purchase_type_id": "BUS_VIBECLIPSE_2024", "price_in_cents": 1200, "festival_id": 1, "max_available": null, "description": "Bus ticket (two-way)", "max_per_account": null },
+    { "purchase_type_id": "SLEEPING_BAG_VIBECLIPSE_2024", "price_in_cents": 1200, "festival_id": 1, "max_available": null, "description": "Sleeping bag", "max_per_account": null },
+    { "purchase_type_id": "ATTENDANCE_VIBECLIPSE_2024", "price_in_cents": 12300, "festival_id": 1, "max_available": 600, "description": "Adult Ticket", "max_per_account": 2 },
+    { "purchase_type_id": "ATTENDANCE_CHILD_VIBECLIPSE_2024", "price_in_cents": 12300, "festival_id": 1, "max_available": null, "description": "Child Ticket", "max_per_account": 4 },
   ],
   volunteer_type: [
-    {"volunteer_type_id":"FAE","description":"Fae"},
-    {"volunteer_type_id":"GENERAL","description":"General volunteer"},
+    { "volunteer_type_id": "FAE", "description": "Fae" },
+    { "volunteer_type_id": "GENERAL", "description": "General volunteer" },
   ],
   age_group: [
-    {"age_group":"BETWEEN_2_AND_18","is_child":true,"description":"Between 2 and 18 years old"},
-    {"age_group":"UNDER_2","is_child":true,"description":"Under 2 years old"},
-    {"age_group":"BETWEEN_18_AND_21","is_child":false,"description":"Between 18 and 21 years old"},
-    {"age_group":"OVER_21","is_child":false,"description":"Over 21 years old"},
+    { "age_group": "BETWEEN_2_AND_18", "is_child": true, "description": "Between 2 and 18 years old" },
+    { "age_group": "UNDER_2", "is_child": true, "description": "Under 2 years old" },
+    { "age_group": "BETWEEN_18_AND_21", "is_child": false, "description": "Between 18 and 21 years old" },
+    { "age_group": "OVER_21", "is_child": false, "description": "Over 21 years old" },
+  ],
+  diet: [
+    { "diet_id": "VEGETARIAN", "description": "Vegetarian" },
+    { "diet_id": "VEGAN", "description": "Vegan" },
   ],
 } as const
