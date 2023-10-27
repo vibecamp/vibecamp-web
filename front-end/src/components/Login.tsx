c3955ce0e8cfd232fe3095319a304fa1263bc356
Brandon Smith <mail@brandons.me>
2023-10-27 18:21:51 -0500

    Add padding back to login page

--- front-end/src/components/Login.tsx
+++ front-end/src/components/Login.tsx
@@ -10,7 +10,7 @@ import { vibefetch } from '../vibefetch'
 import { DEFAULT_FORM_ERROR, preventingDefault } from '../utils'
 import { useObservableState, useRequest, useStable } from '../mobx/hooks'
 import { Form, fieldToProps } from '../mobx/form'
-import LoadingDots from './core/LoadingDots'
+import Col from './core/Col'
 
 export default observer(() => {
     const state = useObservableState({
@@ -51,29 +51,30 @@ export default observer(() => {
 
     return (
         <form className='login' onSubmit={preventingDefault(loginOrSignup.load)}>
-            <Stripes position='top-left' />
+            <Col padding={20}>
+                <Stripes position='top-left' />
 
-            <img src="vibecamp.png" className='logo' />
+                <img src="vibecamp.png" className='logo' />
 
-            <Spacer size={24} />
+                <Spacer size={24} />
 
-            <Input
-                label='Email address'
-                type='email'
-                disabled={loginOrSignup.state.kind === 'loading'}
-                {...fieldToProps(loginForm.fields.emailAddress)}
-            />
+                <Input
+                    label='Email address'
+                    type='email'
+                    disabled={loginOrSignup.state.kind === 'loading'}
+                    {...fieldToProps(loginForm.fields.emailAddress)}
+                />
 
-            <Spacer size={16} />
+                <Spacer size={16} />
 
-            <Input
-                label='Password'
-                type='password'
-                disabled={loginOrSignup.state.kind === 'loading'}
-                {...fieldToProps(loginForm.fields.password)}
-            />
+                <Input
+                    label='Password'
+                    type='password'
+                    disabled={loginOrSignup.state.kind === 'loading'}
+                    {...fieldToProps(loginForm.fields.password)}
+                />
 
-            {loginOrSignup.state.kind === 'error' &&
+                {loginOrSignup.state.kind === 'error' &&
                 <>
                     <Spacer size={8} />
 
@@ -82,22 +83,23 @@ export default observer(() => {
                     </div>
                 </>}
 
-            <Spacer size={24} />
+                <Spacer size={24} />
 
 
-            <Button isSubmit isPrimary isLoading={loginOrSignup.state.kind === 'loading'}>
-                {state.mode === 'login'
-                    ? 'Log in'
-                    : 'Sign up'}
-            </Button>
+                <Button isSubmit isPrimary isLoading={loginOrSignup.state.kind === 'loading'}>
+                    {state.mode === 'login'
+                        ? 'Log in'
+                        : 'Sign up'}
+                </Button>
 
-            <Spacer size={8} />
+                <Spacer size={8} />
 
-            <Button disabled={loginOrSignup.state.kind === 'error'} onClick={() => state.mode = (state.mode === 'login' ? 'signup' : 'login')}>
-                {state.mode === 'login'
-                    ? 'Create an account'
-                    : 'I already have an account'}
-            </Button>
+                <Button disabled={loginOrSignup.state.kind === 'error'} onClick={() => state.mode = (state.mode === 'login' ? 'signup' : 'login')}>
+                    {state.mode === 'login'
+                        ? 'Create an account'
+                        : 'I already have an account'}
+                </Button>
+            </Col>
         </form>
     )
 })
