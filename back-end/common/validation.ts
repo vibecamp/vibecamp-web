f3a834988bee0d0fe044aaa8f424ec3aae514699
Brandon Smith <mail@brandons.me>
2023-10-27 21:36:27 -0500

    Make password requirements a little stronger

--- back-end/common/validation.ts
+++ back-end/common/validation.ts
@@ -12,7 +12,13 @@ export function getPasswordValidationError(val: string) {
     if (val.length === 0) {
         return 'Please enter a password'
     }
-    if (val.length < 6) {
-        return 'Password must be at least six characters'
+    if (val.length < 8) {
+        return 'Password must be at least eight characters'
+    }
+    if (!val.match(/[a-zA-Z]/)) {
+        return 'Password must contain an alphabetic character'
+    }
+    if (!val.match(/[0-9]/)) {
+        return 'Password must contain a numeric character'
     }
 }
