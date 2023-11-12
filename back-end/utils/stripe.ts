f9599b335a19c540ea05720c838bb68f1000615d
Brandon Smith <mail@brandons.me>
2023-11-11 21:48:26 -0600

    Pin stripe API version

--- back-end/utils/stripe.ts
+++ back-end/utils/stripe.ts
@@ -2,5 +2,5 @@ import StripeSDK from 'stripe'
 import env from '../env.ts'
 
 export const stripe = new StripeSDK(env.STRIPE_SECRET_KEY, {
-  apiVersion: '2023-08-16',
+  apiVersion: '2022-11-15',
 })
