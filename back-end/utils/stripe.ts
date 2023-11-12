0f465ece9ff7558223bfe2f3347f240a0e6627e1
Brandon Smith <mail@brandons.me>
2023-11-11 21:51:11 -0600

    Type fix

--- back-end/utils/stripe.ts
+++ back-end/utils/stripe.ts
@@ -2,5 +2,6 @@ import StripeSDK from 'stripe'
 import env from '../env.ts'
 
 export const stripe = new StripeSDK(env.STRIPE_SECRET_KEY, {
-  apiVersion: '2022-11-15',
+  // @ts-ignore Use account-default API version
+  apiVersion: null,
 })
