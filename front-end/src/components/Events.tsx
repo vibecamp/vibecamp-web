fcbd058be59a43151eb2ed209756478a6667f399
Brandon Smith <mail@brandons.me>
2023-12-21 23:19:15 -0600

    Events UI improvements

--- front-end/src/components/Events.tsx
+++ front-end/src/components/Events.tsx
@@ -1,18 +1,18 @@
 import dayjs, { Dayjs } from 'dayjs'
 import React, { FC } from 'react'
 
-import { Tables } from '../../../back-end/types/db-types'
-import { FormValidators } from '../mobx/form'
+import { TABLE_ROWS, Tables } from '../../../back-end/types/db-types'
 import { useObservableClass } from '../mobx/hooks'
 import { observer, setter } from '../mobx/misc'
 import { request } from '../mobx/request'
 import Store from '../stores/Store'
-import { preventingDefault } from '../utils'
+import { fieldProps,preventingDefault, someValue, validate } from '../utils'
 import { vibefetch } from '../vibefetch'
 import Button from './core/Button'
 import Col from './core/Col'
-import DateField from './core/DateField'
+import DateField, { formatNoTimezone } from './core/DateField'
 import Icon from './core/Icon'
+import InfoBlurb from './core/InfoBlurb'
 import Input from './core/Input'
 import LoadingDots from './core/LoadingDots'
 import Modal from './core/Modal'
@@ -27,89 +27,99 @@ type InProgressEvent = {
     description: string,
     start_datetime: Dayjs | null,
     end_datetime: Dayjs | null,
-    location: string | null
+    plaintext_location: string | null,
+    event_site_location: Tables['event_site']['event_site_id'] | null
+}
+
+class EventsScreenState {
+    eventBeingEdited: InProgressEvent | null = null
+    showingEventErrors = false
+
+    filter: 'All' | 'Bookmarked' | 'Mine' = 'All'
+
+    get visibleEvents() {
+        if (this.filter === 'All') {
+            return Store.allEvents.state.result
+        } else if (this.filter === 'Bookmarked') {
+            return Store.allEvents.state.result?.filter(e => Store.bookmarks.state.result?.event_ids.includes(e.event_id))
+        } else if (this.filter === 'Mine') {
+            return Store.allEvents.state.result?.filter(e => e.created_by_account_id === Store.jwtPayload?.account_id)
+        }
+    }
+
+    get eventErrors() {
+        if (this.eventBeingEdited == null) {
+            return {}
+        }
+
+        return validate(this.eventBeingEdited, {
+            name: val => {
+                if (val === '') {
+                    return 'Please enter a name for the event'
+                }
+            },
+            start_datetime: val => {
+                if (val == null) {
+                    return 'Please select a start date/time'
+                }
+            },
+            end_datetime: val => {
+                const start = this.eventBeingEdited?.start_datetime
+                if (start != null && val != null && start >= val) {
+                    return 'End date/time is before start date/time'
+                }
+            }
+        })
+    }
+
+    readonly createNewEvent = () => {
+        this.eventBeingEdited = {
+            event_id: undefined,
+            name: '',
+            description: '',
+            start_datetime: null,
+            end_datetime: null,
+            plaintext_location: null,
+            event_site_location: null
+        }
+    }
+
+    readonly editEvent = (eventId: string) => {
+        const existing = Store.allEvents.state.result?.find(e => e.event_id === eventId)
+
+        if (existing) {
+            this.eventBeingEdited = { ...existing }
+        }
+    }
+
+    readonly saveEvent = request(async () => {
+        this.showingEventErrors = true
+        if (this.eventBeingEdited == null || someValue(this.eventErrors, e => e != null)) {
+            return
+        }
+
+        const { start_datetime, end_datetime } = this.eventBeingEdited
+
+        await vibefetch(Store.jwt, '/event/save', 'post', {
+            event: {
+                ...this.eventBeingEdited,
+                start_datetime: formatNoTimezone(start_datetime!),
+                end_datetime: end_datetime && formatNoTimezone(end_datetime)
+            }
+        })
+        await Store.allEvents.load()
+        this.stopEditingEvent()
+    }, { lazy: true })
+
+    readonly stopEditingEvent = () => {
+        this.eventBeingEdited = null
+    }
 }
 
 export default observer(() => {
-    const state = useObservableClass(class {
-        eventBeingEdited: InProgressEvent | null = null
+    const state = useObservableClass(EventsScreenState)
 
-        filter: 'All' | 'Bookmarked' | 'Mine' = 'All'
-
-        get visibleEvents() {
-            if (this.filter === 'All') {
-                return Store.allEvents.state.result
-            } else if (this.filter === 'Bookmarked') {
-                return Store.allEvents.state.result?.filter(e => Store.bookmarks.state.result?.event_ids.includes(e.event_id))
-            } else if (this.filter === 'Mine') {
-                return Store.allEvents.state.result?.filter(e => e.created_by_account_id === Store.jwtPayload?.account_id)
-            }
-        }
-
-        get eventValidators(): FormValidators<InProgressEvent> {
-            return {
-                name: val => {
-                    if (val === '') {
-                        return 'Please enter a name for the event'
-                    }
-                },
-                start_datetime: val => {
-                    if (val == null) {
-                        return 'Please select a start date/time'
-                    }
-                },
-                end_datetime: val => {
-                    const start = this.eventBeingEdited?.start_datetime
-                    if (start != null && val != null && start >= val) {
-                        return 'End date/time is before start date/time'
-                    }
-                }
-            }
-        }
-
-        readonly createNewEvent = () => {
-            this.eventBeingEdited = {
-                event_id: undefined,
-                name: '',
-                description: '',
-                start_datetime: null,
-                end_datetime: null,
-                location: null
-            }
-        }
-
-        readonly editEvent = (eventId: string) => {
-            const existing = Store.allEvents.state.result?.find(e => e.event_id === eventId)
-
-            if (existing) {
-                this.eventBeingEdited = { ...existing }
-            }
-        }
-
-        readonly saveEvent = request(async () => {
-            if (this.eventBeingEdited == null) {
-                return
-            }
-
-            const { start_datetime, end_datetime } = this.eventBeingEdited
-
-            await vibefetch(Store.jwt, '/event/save', 'post', {
-                event: {
-                    ...this.eventBeingEdited,
-                    start_datetime: start_datetime!.toISOString(),
-                    end_datetime: end_datetime?.toISOString() ?? null
-                }
-            })
-            await Store.allEvents.load()
-            this.stopEditingEvent()
-        }, { lazy: true })
-
-        readonly stopEditingEvent = () => {
-            this.eventBeingEdited = null
-        }
-    })
-
-    const loading = Store.accountInfo.state.kind === 'loading' || Store.allEvents.state.kind === 'loading' || Store.bookmarks.state.kind === 'loading'
+    const loading = Store.accountInfo.state.kind === 'loading' || Store.allEvents.state.kind === 'loading'
     const loadingOrError = loading || Store.accountInfo.state.kind === 'error'
 
     return (
@@ -149,10 +159,7 @@ export default observer(() => {
                         {() =>
                             state.eventBeingEdited != null &&
                                 <EventEditor
-                                    saveEvent={state.saveEvent.load}
-                                    cancel={state.stopEditingEvent}
-                                    isSaving={state.saveEvent.state.kind === 'loading'}
-                                    eventBeingEdited={state.eventBeingEdited}
+                                    eventsScreenState={state}
                                 />}
                     </Modal>
                 </>}
@@ -224,9 +231,7 @@ const Event: FC<{ event: Omit<Tables['event'], 'start_datetime' | 'end_datetime'
                 <Spacer size={8} />
 
                 <Button onClick={state.toggleBookmark} isCompact style={{ width: 'auto' }}>
-                    {state.bookmarked
-                        ? <Icon name='check' style={{ fontSize: '1em' }} />
-                        : <Icon name='star_filled' style={{ fontSize: '1em' }} />}
+                    <Icon name='star' fill={state.bookmarked ? 1 : 0} style={{ fontSize: '1em' }} />
                 </Button>
             </div>
 
@@ -244,7 +249,7 @@ const Event: FC<{ event: Omit<Tables['event'], 'start_datetime' | 'end_datetime'
             <div className='info'>
                 <Icon name='location_on' />
                 <span>
-                    {props.event.location}
+                    {props.event.plaintext_location || (props.event.event_site_location ? TABLE_ROWS['event_site'].find(s => s.event_site_id === props.event.event_site_location)?.name : null)}
                 </span>
             </div>
 
@@ -275,91 +280,154 @@ const Event: FC<{ event: Omit<Tables['event'], 'start_datetime' | 'end_datetime'
     )
 })
 
-const EventEditor = observer((props: { saveEvent: () => void, cancel: () => void, isSaving: boolean, eventBeingEdited: InProgressEvent }) => {
+const EventEditor = observer((props: { eventsScreenState: EventsScreenState }) => {
     const state = useObservableClass(class {
-        locationType: 'Site' | 'Custom' = 'Site'
+        locationType: 'At Vibeclipse' | 'Offsite' = 'At Vibeclipse'
+
+        get isSaving() {
+            return props.eventsScreenState.saveEvent.state.kind === 'loading'
+        }
     })
 
+    if (props.eventsScreenState.eventBeingEdited == null) {
+        return null
+    }
+
+    const selectedSite = TABLE_ROWS['event_site'].find(site => site.event_site_id === props.eventsScreenState.eventBeingEdited?.event_site_location)
+
     return (
-        <form onSubmit={preventingDefault(props.saveEvent)} noValidate>
+        <form onSubmit={preventingDefault(props.eventsScreenState.saveEvent.load)} noValidate>
             <Col padding={20} pageLevel>
                 <Input
                     label='Event name'
-                    disabled={props.isSaving}
-                    value={props.eventBeingEdited.name}
-                    onChange={setter(props.eventBeingEdited, 'name')}
+                    disabled={state.isSaving}
+                    {...fieldProps(
+                        props.eventsScreenState.eventBeingEdited,
+                        'name',
+                        props.eventsScreenState.eventErrors,
+                        props.eventsScreenState.showingEventErrors,
+                    )}
                 />
 
                 <Spacer size={16} />
 
                 <Input
                     label='Event description'
-                    disabled={props.isSaving}
+                    disabled={state.isSaving}
                     multiline
-                    value={props.eventBeingEdited.description}
-                    onChange={setter(props.eventBeingEdited, 'description')}
+                    {...fieldProps(
+                        props.eventsScreenState.eventBeingEdited,
+                        'description',
+                        props.eventsScreenState.eventErrors,
+                        props.eventsScreenState.showingEventErrors,
+                    )}
                 />
 
                 <Spacer size={16} />
 
                 <DateField
                     label='Start'
-                    disabled={props.isSaving}
-                    value={props.eventBeingEdited.start_datetime}
-                    onChange={setter(props.eventBeingEdited, 'start_datetime')}
+                    disabled={state.isSaving}
+                    {...fieldProps(
+                        props.eventsScreenState.eventBeingEdited,
+                        'start_datetime',
+                        props.eventsScreenState.eventErrors,
+                        props.eventsScreenState.showingEventErrors,
+                    )}
                 />
 
                 <Spacer size={16} />
 
                 <DateField
                     label='End'
-                    disabled={props.isSaving}
-                    value={props.eventBeingEdited.end_datetime}
-                    onChange={setter(props.eventBeingEdited, 'end_datetime')}
+                    disabled={state.isSaving}
+                    {...fieldProps(
+                        props.eventsScreenState.eventBeingEdited,
+                        'end_datetime',
+                        props.eventsScreenState.eventErrors,
+                        props.eventsScreenState.showingEventErrors,
+                    )}
                 />
 
                 <Spacer size={16} />
 
+                <InfoBlurb>
+                    Your event can take place at Vibeclipse, or it can take
+                    place before/after.
+                    <br /><br />
+                    Vibeclipse camp site locations have limited capacity, and
+                    scheduling will be first-come-first-serve for a given place
+                    + time.
+                </InfoBlurb>
+
+                <Spacer size={16} />
+
                 <RowSelect
-                    options={['Site', 'Custom']}
+                    label='My event will be...'
+                    options={['At Vibeclipse', 'Offsite']}
                     value={state.locationType}
                     onChange={setter(state, 'locationType')}
                 />
 
                 <Spacer size={16} />
 
-                {state.locationType === 'Site'
+                {state.locationType === 'At Vibeclipse'
                     ? <>
                         <RadioGroup
                             options={Store.allEventSites.state.result?.map(s => ({
-                                value: s.name,
+                                value: s.event_site_id,
                                 label: s.name
                             })) ?? []}
-                            value={props.eventBeingEdited.location ?? ''}
-                            onChange={setter(props.eventBeingEdited, 'location')}
+                            directon='row'
+                            {...fieldProps(
+                                props.eventsScreenState.eventBeingEdited,
+                                'event_site_location',
+                                props.eventsScreenState.eventErrors,
+                                props.eventsScreenState.showingEventErrors,
+                            )}
                         />
                     </>
                     : <Input
                         label='Location'
-                        disabled={props.isSaving}
-                        value={props.eventBeingEdited.location ?? ''}
-                        onChange={setter(props.eventBeingEdited, 'location')}
+                        disabled={state.isSaving}
+                        multiline
+                        {...fieldProps(
+                            props.eventsScreenState.eventBeingEdited,
+                            'plaintext_location',
+                            props.eventsScreenState.eventErrors,
+                            props.eventsScreenState.showingEventErrors,
+                        )}
+                        value={props.eventsScreenState.eventBeingEdited.plaintext_location ?? ''}
                     />}
 
-                <Spacer size={16} />
+                <Spacer size={8} />
 
-                <Button isSubmit isPrimary isLoading={props.isSaving}>
-                    {props.eventBeingEdited.event_id == null
+                {selectedSite &&
+                    <EventSiteInfo eventSite={selectedSite} />}
+
+                <Spacer size={8} />
+
+                <Button isSubmit isPrimary isLoading={state.isSaving}>
+                    {props.eventsScreenState.eventBeingEdited.event_id == null
                         ? 'Create event'
                         : 'Save event'}
                 </Button>
 
                 <Spacer size={8} />
 
-                <Button onClick={props.cancel} disabled={props.isSaving}>
+                <Button onClick={props.eventsScreenState.stopEditingEvent} disabled={state.isSaving}>
                     Cancel
                 </Button>
             </Col>
         </form>
+    )
+})
+
+const EventSiteInfo = observer((props: { eventSite: Tables['event_site'] }) => {
+
+    return (
+        <div>
+            {props.eventSite.description}
+        </div>
     )
 })
