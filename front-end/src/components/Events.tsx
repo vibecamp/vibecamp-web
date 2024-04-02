f1962900cd6ed3eab9b8e2934dc4af53328e3398
Brandon Smith <mail@brandons.me>
2024-04-02 17:37:56 -0500

    Temporary fix for events in the near future not showing

--- front-end/src/components/Events.tsx
+++ front-end/src/components/Events.tsx
@@ -51,7 +51,7 @@ class EventsScreenState {
 
     get visibleEvents() {
         if (this.filter === 'All') {
-            return Store.allEvents.state.result?.filter(e => e.start_datetime.isAfter(dayjs()))
+            return Store.allEvents.state.result?.filter(e => e.start_datetime.subtract(1, 'day').isAfter(dayjs()))
         } else if (this.filter === 'Bookmarked') {
             return this.bookmarkedEvents
         } else if (this.filter === 'Mine') {
