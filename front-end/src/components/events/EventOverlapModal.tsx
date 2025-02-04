1b998ca9ecab63637ec00e6436e5ae275887e3bc
Dario Sanchez <dario.zyx@gmail.com>
2025-02-03 22:44:08 -0500

    Improve styles for event overlap modal table

--- front-end/src/components/events/EventOverlapModal.tsx
+++ front-end/src/components/events/EventOverlapModal.tsx
@@ -20,24 +20,74 @@ export default React.memo(({ isOpen, onClose, onConfirm, overlappingEvents }: Pr
                 <Col align='center' justify='center' padding={20} pageLevel>
                     <div style={{ fontSize: 22, textAlign: 'center' }}>
                         This event overlaps with the following. Schedule it anyway?
-
-                        <Spacer size={16} />
-
-                        {overlappingEvents.map(event => (
-                            <div key={event.event_id} style={{ fontSize: 16, marginTop: 8 }}>
-                                &quot;{event.name}&quot;
-                                <br />
-                                {event.start_datetime.format('MMM D, h:mm A')} -{' '}
-                                {event.end_datetime?.format('h:mm A') ?? 'onwards'}
-                                <br />
-                                {event.event_site_location_name}
-                            </div>
-                        ))}
-
-                        <Spacer size={16} />
-
-                        Want to schedule it anyway?
                     </div>
+
+                    <Spacer size={16} />
+
+                    <table style={{ tableLayout: 'fixed', width: '100%', borderSpacing: 0 }}>
+                        <thead>
+                            <tr>
+                                <th
+                                    style={{
+                                        padding: '5px 10px',
+                                        width: '100px',
+                                        textAlign: 'left',
+                                    }}
+                                >
+                                    When
+                                </th>
+                                <th style={{ padding: '5px 10px', textAlign: 'left' }}>
+                                    What
+                                </th>
+                                <th style={{ padding: '5px 10px', textAlign: 'left' }}>
+                                    Where
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {overlappingEvents.map((event, index) => (
+                                <tr
+                                    key={event.event_id}
+                                    style={{
+                                        backgroundColor:
+                                            index % 2 ? 'transparent' : 'rgba(0, 0, 0, 0.1)',
+                                    }}
+                                >
+                                    <td
+                                        style={{
+                                            padding: '5px 10px',
+                                            whiteSpace: 'nowrap',
+                                            overflow: 'hidden',
+                                            textOverflow: 'ellipsis',
+                                            width: '100px',
+                                        }}
+                                    >
+                                        {event.start_datetime.format('ddd h:mma')}
+                                    </td>
+                                    <td
+                                        style={{
+                                            padding: '5px 10px',
+                                            whiteSpace: 'nowrap',
+                                            overflow: 'hidden',
+                                            textOverflow: 'ellipsis',
+                                        }}
+                                    >
+                                        {event.name}
+                                    </td>
+                                    <td
+                                        style={{
+                                            padding: '5px 10px',
+                                            whiteSpace: 'nowrap',
+                                            overflow: 'hidden',
+                                            textOverflow: 'ellipsis',
+                                        }}
+                                    >
+                                        {event.event_site_location_name}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
 
                     <Spacer size={16} />
 
