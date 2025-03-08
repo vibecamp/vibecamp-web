a1468a7eef91f2a5dc3b7fd84857aa3ae43d8df1
Brandon Smith <mail@brandons.me>
2025-03-08 15:13:28 -0600

    Update db-types

--- back-end/types/db-types.ts
+++ back-end/types/db-types.ts
@@ -6,12 +6,17 @@
 // Nominal types for primary keys; explanation of this pattern: https://www.typescriptlang.org/play/typescript/language-extensions/nominal-typing.ts.html
 export const account__account_id_TypeSymbol = Symbol('account__account_id_Type'); export type account__account_id_Type = string & { [account__account_id_TypeSymbol]: undefined };
 export const account__application_id_TypeSymbol = Symbol('account__application_id_Type'); export type account__application_id_Type = string & { [account__application_id_TypeSymbol]: undefined };
+export const announcement__announcement_id_TypeSymbol = Symbol('announcement__announcement_id_Type'); export type announcement__announcement_id_Type = string & { [announcement__announcement_id_TypeSymbol]: undefined };
 export const application__application_id_TypeSymbol = Symbol('application__application_id_Type'); export type application__application_id_Type = string & { [application__application_id_TypeSymbol]: undefined };
 export const attendee__associated_account_id_TypeSymbol = Symbol('attendee__associated_account_id_Type'); export type attendee__associated_account_id_Type = string & { [attendee__associated_account_id_TypeSymbol]: undefined };
 export const attendee__attendee_id_TypeSymbol = Symbol('attendee__attendee_id_Type'); export type attendee__attendee_id_Type = string & { [attendee__attendee_id_TypeSymbol]: undefined };
 export const attendee_cabin__attendee_id_TypeSymbol = Symbol('attendee_cabin__attendee_id_Type'); export type attendee_cabin__attendee_id_Type = string & { [attendee_cabin__attendee_id_TypeSymbol]: undefined };
 export const attendee_cabin__cabin_id_TypeSymbol = Symbol('attendee_cabin__cabin_id_Type'); export type attendee_cabin__cabin_id_Type = string & { [attendee_cabin__cabin_id_TypeSymbol]: undefined };
 export const attendee_cabin__festival_id_TypeSymbol = Symbol('attendee_cabin__festival_id_Type'); export type attendee_cabin__festival_id_Type = string & { [attendee_cabin__festival_id_TypeSymbol]: undefined };
+export const badge_info__attendee_id_TypeSymbol = Symbol('badge_info__attendee_id_Type'); export type badge_info__attendee_id_Type = string & { [badge_info__attendee_id_TypeSymbol]: undefined };
+export const badge_info__badge_info_id_TypeSymbol = Symbol('badge_info__badge_info_id_Type'); export type badge_info__badge_info_id_Type = string & { [badge_info__badge_info_id_TypeSymbol]: undefined };
+export const badge_info__badge_picture_image_id_TypeSymbol = Symbol('badge_info__badge_picture_image_id_Type'); export type badge_info__badge_picture_image_id_Type = string & { [badge_info__badge_picture_image_id_TypeSymbol]: undefined };
+export const badge_info__festival_id_TypeSymbol = Symbol('badge_info__festival_id_Type'); export type badge_info__festival_id_Type = string & { [badge_info__festival_id_TypeSymbol]: undefined };
 export const cabin__cabin_id_TypeSymbol = Symbol('cabin__cabin_id_Type'); export type cabin__cabin_id_Type = string & { [cabin__cabin_id_TypeSymbol]: undefined };
 export const cabin__festival_site_id_TypeSymbol = Symbol('cabin__festival_site_id_Type'); export type cabin__festival_site_id_Type = string & { [cabin__festival_site_id_TypeSymbol]: undefined };
 export const diet__diet_id_TypeSymbol = Symbol('diet__diet_id_Type'); export type diet__diet_id_Type = string & { [diet__diet_id_TypeSymbol]: undefined };
@@ -24,6 +29,8 @@ export const event_bookmark__event_id_TypeSymbol = Symbol('event_bookmark__event
 export const event_site__event_site_id_TypeSymbol = Symbol('event_site__event_site_id_Type'); export type event_site__event_site_id_Type = string & { [event_site__event_site_id_TypeSymbol]: undefined };
 export const event_site__festival_site_id_TypeSymbol = Symbol('event_site__festival_site_id_Type'); export type event_site__festival_site_id_Type = string & { [event_site__festival_site_id_TypeSymbol]: undefined };
 export const event_type__event_type_id_TypeSymbol = Symbol('event_type__event_type_id_Type'); export type event_type__event_type_id_Type = string & { [event_type__event_type_id_TypeSymbol]: undefined };
+export const faq_node__faq_node_id_TypeSymbol = Symbol('faq_node__faq_node_id_Type'); export type faq_node__faq_node_id_Type = string & { [faq_node__faq_node_id_TypeSymbol]: undefined };
+export const faq_node__parent_faq_node_id_TypeSymbol = Symbol('faq_node__parent_faq_node_id_Type'); export type faq_node__parent_faq_node_id_Type = string & { [faq_node__parent_faq_node_id_TypeSymbol]: undefined };
 export const festival__festival_id_TypeSymbol = Symbol('festival__festival_id_Type'); export type festival__festival_id_Type = string & { [festival__festival_id_TypeSymbol]: undefined };
 export const festival__festival_site_id_TypeSymbol = Symbol('festival__festival_site_id_Type'); export type festival__festival_site_id_Type = string & { [festival__festival_site_id_TypeSymbol]: undefined };
 export const festival_site__festival_site_id_TypeSymbol = Symbol('festival_site__festival_site_id_Type'); export type festival_site__festival_site_id_Type = string & { [festival_site__festival_site_id_TypeSymbol]: undefined };
@@ -35,6 +42,8 @@ export const purchase__purchase_id_TypeSymbol = Symbol('purchase__purchase_id_Ty
 export const purchase__purchase_type_id_TypeSymbol = Symbol('purchase__purchase_type_id_Type'); export type purchase__purchase_type_id_Type = string & { [purchase__purchase_type_id_TypeSymbol]: undefined };
 export const purchase_type__festival_id_TypeSymbol = Symbol('purchase_type__festival_id_Type'); export type purchase_type__festival_id_Type = string & { [purchase_type__festival_id_TypeSymbol]: undefined };
 export const purchase_type__purchase_type_id_TypeSymbol = Symbol('purchase_type__purchase_type_id_Type'); export type purchase_type__purchase_type_id_Type = string & { [purchase_type__purchase_type_id_TypeSymbol]: undefined };
+export const stored_image__owned_by_account_id_TypeSymbol = Symbol('stored_image__owned_by_account_id_Type'); export type stored_image__owned_by_account_id_Type = string & { [stored_image__owned_by_account_id_TypeSymbol]: undefined };
+export const stored_image__stored_image_id_TypeSymbol = Symbol('stored_image__stored_image_id_Type'); export type stored_image__stored_image_id_Type = string & { [stored_image__stored_image_id_TypeSymbol]: undefined };
 export const volunteer_type__volunteer_type_id_TypeSymbol = Symbol('volunteer_type__volunteer_type_id_Type'); export type volunteer_type__volunteer_type_id_Type = string & { [volunteer_type__volunteer_type_id_TypeSymbol]: undefined };
 
 export type Tables = {
@@ -51,6 +60,12 @@ export type Tables = {
     password_salt: string | null,
   },
   age_range: (typeof TABLE_ROWS)['age_range'][number]
+  announcement: {
+    announced_at: Date,
+    announcement_id: announcement__announcement_id_Type,
+    content: string,
+    title: string,
+  },
   application: {
     anything_else: string,
     application_id: application__application_id_Type,
@@ -99,6 +114,19 @@ export type Tables = {
     cabin_id: Tables['cabin']['cabin_id'],
     festival_id: Tables['festival']['festival_id'],
   },
+  badge_info: {
+    attended_vc_1: boolean | null,
+    attended_vc_2: boolean | null,
+    attendee_id: Tables['attendee']['attendee_id'],
+    badge_bio: string | null,
+    badge_info_id: badge_info__badge_info_id_Type,
+    badge_location: string | null,
+    badge_name: string,
+    badge_picture_image_id: Tables['stored_image']['stored_image_id'] | null,
+    badge_picture_url: string | null,
+    badge_username: string | null,
+    festival_id: Tables['attendee_cabin']['festival_id'],
+  },
   cabin: {
     cabin_id: cabin__cabin_id_Type,
     festival_site_id: Tables['festival_site']['festival_site_id'],
@@ -142,6 +170,13 @@ export type Tables = {
     theme: string | null,
   },
   event_type: (typeof TABLE_ROWS)['event_type'][number]
+  faq_node: {
+    content: string | null,
+    faq_node_id: faq_node__faq_node_id_Type,
+    order: number | null,
+    parent_faq_node_id: faq_node__parent_faq_node_id_Type | null,
+    title: string,
+  },
   festival: {
     email_banner_image: string | null,
     end_date: Date,
@@ -186,6 +221,11 @@ export type Tables = {
     price_in_cents: number,
     purchase_type_id: purchase_type__purchase_type_id_Type,
   },
+  stored_image: {
+    image_data: unknown,
+    owned_by_account_id: Tables['account']['account_id'] | null,
+    stored_image_id: stored_image__stored_image_id_Type,
+  },
   volunteer_type: (typeof TABLE_ROWS)['volunteer_type'][number]
 }
 
@@ -216,9 +256,11 @@ export const TABLE_ROWS = {
 export const TABLE_COLUMNS = {
   account: ["account_id","application_id","email_address","is_authorized_to_buy_tickets","is_low_income","is_seed_account","is_team_member","notes","password_hash","password_salt"],
   age_range: ["age_range","description","end","start"],
+  announcement: ["announced_at","announcement_id","content","title"],
   application: ["anything_else","application_id","attractive_virtues","experiences_hoping_to_share","group_activity","hoping_to_get_out_of_the_festival","how_found_out","identify_as","interested_in_volunteering","is_accepted","last_conversation","looking_forward_to_conversations","name","previous_events","strongest_virtues","submitted_on","twitter_handle"],
   attendee: ["age","age_range","associated_account_id","attendee_id","diet","discord_handle","has_allergy_eggs","has_allergy_fish","has_allergy_milk","has_allergy_peanuts","has_allergy_shellfish","has_allergy_soy","has_allergy_tree_nuts","has_allergy_wheat","interested_in_pre_call","interested_in_volunteering_as","is_primary_for_account","medical_training","name","notes","planning_to_camp","twitter_handle"],
   attendee_cabin: ["attendee_id","cabin_id","festival_id"],
+  badge_info: ["attended_vc_1","attended_vc_2","attendee_id","badge_bio","badge_info_id","badge_location","badge_name","badge_picture_image_id","badge_picture_url","badge_username","festival_id"],
   cabin: ["cabin_id","festival_site_id","max_occupancy","name","nickname","notes"],
   diet: ["description","diet_id"],
   discount: ["discount_code","discount_id","price_multiplier","purchase_type_id"],
@@ -226,10 +268,12 @@ export const TABLE_COLUMNS = {
   event_bookmark: ["account_id","event_id"],
   event_site: ["can_host_multiple_events","description","equipment","event_site_id","festival_site_id","location","name","people_cap","structure_type","theme"],
   event_type: ["event_type_id"],
+  faq_node: ["content","faq_node_id","order","parent_faq_node_id","title"],
   festival: ["email_banner_image","end_date","festival_id","festival_name","festival_site_id","info_url","sales_are_open","start_date"],
   festival_site: ["festival_site_id","festival_site_name","location"],
   invite_code: ["code","created_by_account_id","festival_id","used_by_account_id"],
   purchase: ["applied_discount","checked_in","is_test_purchase","owned_by_account_id","purchase_id","purchase_type_id","purchased_on","stripe_payment_intent"],
   purchase_type: ["available_from","available_to","description","festival_id","hidden_from_ui","is_attendance_ticket","low_income_only","max_available","max_per_account","price_in_cents","purchase_type_id"],
+  stored_image: ["image_data","owned_by_account_id","stored_image_id"],
   volunteer_type: ["description","volunteer_type_id"],
 } as const
