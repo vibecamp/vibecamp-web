986f9b1aa70386c921c36a36d334d74476671dbc
Elija Sorensen <elija@hey.com>
2025-12-29 13:40:07 -0600

    jwt security hotfix

--- back-end/routes/v1/event.ts
+++ back-end/routes/v1/event.ts
@@ -26,6 +26,7 @@ export default function register(router: Router) {
   defineRoute(router, {
     endpoint: '/events',
     method: 'get',
+    requireAuth: true,
     handler: async () => {
       const events = await getAllEvents()
 
