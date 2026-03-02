a4609b20febf11a19c36e185d7bab3d18658d9c4
Brandon Smith <mail@brandons.me>
2026-03-01 19:42:56 -0600

    Revert change that tripped over Next.js's bullshit semantics

--- front-end/src/env.ts
+++ front-end/src/env.ts
@@ -1,14 +1,5 @@
-function requireEnvVar(name: string) {
-    const value = process.env[name]
-
-    if (!value) {
-        throw Error(`Expected environment variable ${name} wasn't found`)
-    }
-
-    return value
-}
 
 export default {
-    BACK_END_ORIGIN: requireEnvVar('NEXT_PUBLIC_BACK_END_ORIGIN'),
-    STRIPE_PUBLIC_KEY: requireEnvVar('NEXT_PUBLIC_STRIPE_PUBLIC_KEY'),
+    BACK_END_ORIGIN: process.env.NEXT_PUBLIC_BACK_END_ORIGIN,
+    STRIPE_PUBLIC_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY,
 }
