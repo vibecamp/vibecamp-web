6fc959b058917c949ebffa7f9593f6be62a012f0
Brandon Smith <mail@brandons.me>
2026-03-01 19:45:46 -0600

    Add checks that don't conflict with Next.js

--- front-end/src/env.ts
+++ front-end/src/env.ts
@@ -1,5 +1,11 @@
 
+const BACK_END_ORIGIN = process.env.NEXT_PUBLIC_BACK_END_ORIGIN
+if (!BACK_END_ORIGIN) throw Error('Expected environment variable NEXT_PUBLIC_BACK_END_ORIGIN not found')
+
+const STRIPE_PUBLIC_KEY = process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY
+if (!STRIPE_PUBLIC_KEY) throw Error('Expected environment variable NEXT_PUBLIC_STRIPE_PUBLIC_KEY not found')
+
 export default {
-    BACK_END_ORIGIN: process.env.NEXT_PUBLIC_BACK_END_ORIGIN!,
-    STRIPE_PUBLIC_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!,
+    BACK_END_ORIGIN,
+    STRIPE_PUBLIC_KEY,
 }
